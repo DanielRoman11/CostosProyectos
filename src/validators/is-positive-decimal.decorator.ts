622c8e8d42cb78300c18889b4e3f72f8a05f9eaa
Daniel Román <danielroman.05111@gmail.com>
2024-08-12 14:07:26 -0500

    feat: IsPostiveDecimal Decorator

---
+++ src/validators/is-positive-decimal.decorator.ts
@@ -0,0 +1,30 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'isPositiveDecimal', async: false })
+class IsPositiveDecimalConstraint implements ValidatorConstraintInterface {
+  validate(value: string): Promise<boolean> | boolean {
+    const numberValue = parseFloat(value);
+    return !isNaN(numberValue) && numberValue > 0;
+  }
+  defaultMessage?(): string {
+    throw new Error('El valor debe ser un número decimal positivo.');
+  }
+}
+
+export function IsPositiveDecimal(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'isPositiveDecimal',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsPositiveDecimalConstraint,
+    });
+  };
+}
