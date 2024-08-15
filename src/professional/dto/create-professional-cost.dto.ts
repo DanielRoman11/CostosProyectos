2df87923431081f25cead80b3f5c3c1e936c6835
Daniel Román <88067941+DanielRoman11@users.noreply.github.com>
2024-08-15 08:56:31 +0000

    fix: CreateProfessionalCostDetailDto error sorting

--- src/professional/dto/create-professional-cost.dto.ts
+++ src/professional/dto/create-professional-cost.dto.ts
@@ -4,8 +4,13 @@ import { Professional } from '../entities/profesional.entity';
 import { Project } from 'src/projects/entities/project.entity';
 
 export class CreateProfessionalCostDetailDto {
-  @IsNotEmpty({ message: 'La unidad de medida no puede estar vacia' })
-  unit: string;
+  @IsNotEmpty({ message: 'Debe seleccionar un projecto obligatoriamente' })
+  project: Project;
+
+  @IsNotEmpty({
+    message: 'Debe seleccionar los profesionales para calcular los costos',
+  })
+  professional_ids: Pick<Professional, 'id'>[];
 
   @IsNotEmpty({ message: 'El precio unitario no puede estar vacío' })
   @IsDecimal(
@@ -17,11 +22,6 @@ export class CreateProfessionalCostDetailDto {
   @IsPositiveDecimal()
   quantity: string;
 
-  @IsNotEmpty({
-    message: 'Debe seleccionar los profesionales para calcular los costos',
-  })
-  professional_ids: Pick<Professional, 'id'>[];
-
-  @IsNotEmpty({ message: 'Debe seleccionar un projecto obligatoriamente' })
-  project: Project;
+  @IsNotEmpty({ message: 'La unidad de medida no puede estar vacia' })
+  unit: string;
 }
