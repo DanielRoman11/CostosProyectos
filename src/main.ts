226dd3c1441b6323f9c7f5b44f47ea2a6e7288e8
Daker <danielroman.05111@gmail.com>
2024-12-08 19:11:03 -0500

    feat: Main.ts, Usando useContainer para poder usar validaciones asíncronas

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,16 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  app.useGlobalPipes(new ValidationPipe());
-
-	app.enableCors({origin: ['http://localhost:3000','http://127.0.0.1'] })
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors({ origin: ['http://localhost:3000', 'http://127.0.0.1'] });
 
   await app.listen(3001, '0.0.0.0');
 }
